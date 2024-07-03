77449ac9854486e79ba42613a1e7d1610dbc84dc
VPresich <valya.alexandrova.presich@gmail.com>
2024-07-03 19:56:30 +0200

    added persistence

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,42 @@
 import { configureStore } from "@reduxjs/toolkit";
 import campersReducer from "../redux/campers/slice";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+
+// export const store = configureStore({
+//   reducer: {
+//     campers: campersReducer,
+//   },
+// });
+
+const campersPersistConfig = {
+  key: "campers",
+  storage,
+  whitelist: ["favorites"],
+};
+
 export const store = configureStore({
   reducer: {
-    campers: campersReducer,
+    campers: persistReducer(campersPersistConfig, campersReducer),
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
