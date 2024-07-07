b80a5143b4feec15ad61404dc3e7f90d0308c103
VPresich <valya.alexandrova.presich@gmail.com>
2024-07-07 13:20:38 +0200

    fixed radio button

--- src/components/CardTitle/CardTitle.jsx
+++ src/components/CardTitle/CardTitle.jsx
@@ -1,12 +1,13 @@
 import css from "./CardTitle.module.css";
 import FavoriteButton from "../UI/FavoriteButton/FavoriteButton";
+import EllipsisText from "../UI/EllipsisText/EllipsisText";
 import formatPrice from "../../auxiliary/formatPrice";
 
 export default function CardTitle({ id, name, price }) {
   return (
     <div className={css.container}>
       <div className={css.titleWrapper}>
-        <h3 className={css.title}>{name}</h3>
+        <EllipsisText text={name} maxLines={1} className={css.title} />
         <p className={css.price}>{formatPrice(price)}</p>
       </div>
       <FavoriteButton id={id} />
