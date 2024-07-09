60d4cfefd7b83ba7e85dccb2af3763b90759f959
VPresich <valya.alexandrova.presich@gmail.com>
2024-07-09 20:59:44 +0200

    added google maps

--- src/redux/geocode/selectors.js
+++ src/redux/geocode/selectors.js
@@ -1,1 +1,13 @@
-export const selectCoordinates = (state) => state.campers.coordinates;
+import { createSelector } from "reselect";
+
+const selectGeoState = (state) => state.geocode;
+
+export const selectGeoCoords = createSelector(
+  selectGeoState,
+  (geoState) => geoState.coordinates
+);
+
+export const selectIsLoading = createSelector(
+  selectGeoState,
+  (geoState) => geoState.isLoading
+);
